e3eac3e07da95b1ed8a8f263aa05129247394c04
Diego <solollamamepatodonald@gmail.com>
2012-10-07 17:41:04 -0400

    modificacion de entidad.lua

---
+++ entidad.lua
@@ -0,0 +1,17 @@
+local ent = {}
+
+ent.x = 0
+ent.y = 0
+ent.velX = 0
+ent.velY = 0
+ent.grad = 0
+ent.sprites = {}
+ent.estados = {}
+
+
+
+ent.estatico = true
+ent.activo = true
+ent.contolable = true
+ent.tocable = true
+
