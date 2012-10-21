51cea3d9bae9f3fb3ea81d727634e871bd6b3c7d
Diego <solollamamepatodonald@gmail.com>
2012-10-21 16:37:14 -0300

    comit de hoy

--- entidad.lua
+++ entidad.lua
@@ -1,19 +1,27 @@
 local ent = {}
 
-ent.x = 0
-ent.y = 0
-ent.velX = 0
-ent.velY = 0
-ent.grad = 0
-ent.sprites = {}
+ent.x = 0 -- posicion x de la entidad
+ent.y = 0 -- posicion y de la entidad
+ent.velX = 0 -- velocidad x de la entidad
+ent.velY = 0 -- velocidad y de la entidad
+ent.grad = 0 -- grado de rotacion de la entidad
+ent.sprites = {} --sprites de la entidad (hashmap <string, imgSprite>)
+ent.estados = {}
 ent.estado = "normal"
 ent.posSprite = 0
 ent.color = {255,255,255,255}
-
-
+--posibles estados de la entidad
 ent.estatico = true
 ent.activo = true
 ent.contolable = true
 ent.tocable = true
+
+function ent:update(dt) 
+end
+
+function ent:draw()
+end
+
+
 
 return ent
